455ed00e806bc3766666e316a220d0fe58726be3
Farrukh Hafeez <chaudharyfarrukh02@gmail.com>
2024-06-30 01:41:38 +0500

    Project Structure

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            // This needs to be mirrored in tsconfig.json
+            '@assets': './assets',
+            '@components': './src/components',
+            '@navigation': './src/navigation',
+            '@shared': './src/shared',
+            '@screens': './src/screens',
+          },
+        },
+      ],
+      ['module:react-native-dotenv', {moduleName: '@env', path: '.env'}],
+    ],
+  };
+};
